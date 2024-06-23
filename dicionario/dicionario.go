6738f53ad2047fa901b10792f63e2dcb4cb2b98a
KauanMesquita <kauancontact@gmail.com>
2024-06-22 21:24:51 -0300

    refatoring

--- dicionario/dicionario.go
+++ dicionario/dicionario.go
@@ -1,6 +1,16 @@
 package main
 
-func Busca(dicionario map[string]string, palavra string) string {
-	return dicionario[palavra]
+import "errors"
 
+type Dicionario map[string]string
+
+var ErrNaoEncontrado = errors.New("não foi possível encontrar a palavra")
+
+func (d Dicionario) Busca(palavra string) (string, error) {
+	definicao, existe := d[palavra]
+	if !existe {
+		return "", ErrNaoEncontrado
+	}
+
+	return definicao, nil
 }
--- dicionario/dicionario_test.go
+++ dicionario/dicionario_test.go
@@ -3,11 +3,26 @@ package main
 import "testing"
 
 func TestBusca(t *testing.T) {
-	dicionario := map[string]string{"teste": "isso é apenas um teste"}
+	dicionario := Dicionario{"teste": "isso é apenas um teste"}
 
-	resultado := Busca(dicionario, "teste")
-	esperado := "isso é apenas um teste"
+	t.Run("palavra conhecida", func(t *testing.T) {
+		resultado, _ := dicionario.Busca("teste")
+		esperado := "isso é apenas um teste"
 
+		comparaStrings(t, resultado, esperado)
+	})
+
+	t.Run("palavra desconhecida", func(t *testing.T) {
+		_, err := dicionario.Busca("desconhecida")
+
+		if err == nil {
+			t.Fatal("é esperado que um erro desja obtido")
+		}
+
+	})
+}
+
+func comparaStrings(t *testing.T, resultado, esperado string) {
 	if resultado != esperado {
 		t.Errorf("resultado '%s', esperado '%s', dado '%s'", resultado, esperado, "test")
 	}
